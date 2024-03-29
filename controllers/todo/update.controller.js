1b94d4663453dddccf972d82b12cedfab42146cd
unknown <subhransusekharsahu29@gmail.com>
2024-03-29 11:23:51 +0530

    done final

--- controllers/todo/update.controller.js
+++ controllers/todo/update.controller.js
@@ -3,7 +3,7 @@ const TodoModel = require('../../models/todo.model.js')
 const updateContoller = async (req, res) => {
     const { id } = req.params;
     const update = req.body;
-    
+
     console.log(req.params, req.body);
     // Check if ID and update data are provided
     if (!id) {
@@ -14,14 +14,15 @@ const updateContoller = async (req, res) => {
     }
 
     try {
-        const updateData = await TodoModel.findByIdAndUpdate(id, update);
+        const updateData = await TodoModel.findOneAndUpdate({ "_id": id }, update);
+        console.log(updateData)
         if (updateData) {
             res.status(200).json({
                 "status": 200,
-                "data": updateData
+                "message": "data updated successfully."
             }); // Send the updated document as a JSON response
         } else {
-            res.status(404).json({ error: 'Document not found' }); // If document not found
+            res.status(500).json({ error: 'Failed to update the data' }); // If document not found
         }
 
     } catch (error) {
