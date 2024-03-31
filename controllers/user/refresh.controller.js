a226cada9d23d04aa1467657c559b2f27a335a4a
unknown <subhransusekharsahu29@gmail.com>
2024-03-31 11:49:41 +0530

    edited

--- controllers/user/refresh.controller.js
+++ controllers/user/refresh.controller.js
@@ -17,20 +17,14 @@ const RefreshTokenController = async (req, res) => {
         //if user is not available
         if (!userData) { return res.status(403) } //forbidden
         //user is available so verify the jwt token if its working or not
-        // jwt.verify(
-        //     refreshToken,
-        //     process.env.REFRESH_TOKEN_SECRET,
-        //     (err, decoded) => {
-        //         if (err || userData.name !== decoded.user) {
-        //             return res.status(403)
-        //         }
-        //         
-        //     }
-        // )
 
         const checkJwt = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        if(!checkJwt){
+           return res.sendStatus(403);
+        }
         const accessToken = accessTokenGenerator(userData.name)
-        return res.json({ accessToken });
+        return res.status(200).json({ accessToken });
+        
     } catch (error) {
         return res.status(500).json({
             "message": error.message,
